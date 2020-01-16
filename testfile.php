80dafa8c1764e523a1eec8b29cf49bbbbd9ad010
KlarDak <mk_daking@mail.ru>
2020-01-16 07:31:03 +0300

    Telegram Bot 0.1.3

--- testfile.php
+++
@@ -1,6 +0,0 @@
-<?php
-  require __DIR__ . "/vendor/autoload.php";
-
-  $bot = new KlarDak\TelegramBot\Entities\Chat(["id" => 1, "type" => "group", "photo" => []]);
-  var_dump($bot->getPhoto());
-?>
