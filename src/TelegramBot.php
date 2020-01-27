4f3380afb13ea7d87096dc7044058b77c5d94f2b
KlarDak <mk_daking@mail.ru>
2020-01-27 22:14:25 +0300

    Telegram Bot 0.4.2.2
    Some changes...

--- src/TelegramBot.php
+++ src/TelegramBot.php
@@ -1,33 +1,9 @@
 <?php
   namespace KlarDak\TelegramBot;
 
-  class TelegramBot
+  class TelegramBot extends LibFunctions
   {
     public static $token = "";
-
-    public $update_id = 0;
-
-    public $message_id = 0;
-    public $chat_id = 0;
-    public $user_id = 0;
-
-    public $from;
-    public $chat;
-    public $text;
-    public $caption;
-    public $date = 0;
-    public $photo;
-    public $video;
-    public $animation;
-    public $document;
-    public $voice;
-    public $audio;
-    public $contact;
-    public $venue;
-    public $location;
-    public $videonote;
-    public $poll;
-    public $sticker;
 
     function __construct(string $token, bool $getupdates = false)
     {
@@ -45,35 +21,6 @@
     public function getUpdates(int $offset = 0, int $limit = 10, int $timeout = 0, array $allowed_updates = ["message", "callback_query", "inline_query"])
     {
       return self::doAction("getUpdates", ["offset" => $offset, "limit" => $limit, "timeout" => $timeout, "allowed_updates", $allowed_updates]);
-    }
-
-    private function getUpdate()
-    {
-      $data = json_decode(file_get_contents("php://input"), true);
-
-      $this->update_id = $data["update_id"];
-      $this->message_id = $data["message"]["message_id"];
-      $this->user_id = $data["message"]["from"]["id"];
-      $this->chat_id = $data["message"]["chat"]["id"];
-      $this->from = $data["message"]["from"];
-      $this->chat = $data["message"]["chat"];
-      $this->text = $data["message"]["text"];
-      $this->caption = $data["message"]["caption"];
-      $this->date = $data["message"]["date"];
-      $this->photo = $data["message"]["photo"];
-      $this->video = $data["message"]["video"];
-      $this->animation = $data["message"]["animation"];
-      $this->audio = $data["message"]["audio"];
-      $this->document = $data["message"]["document"];
-      $this->voice = $data["message"]["voice"];
-      $this->videonote = $data["message"]["video_note"];
-      $this->contact = $data["message"]["contact"];
-      $this->location = $data["message"]["location"];
-      $this->venue = $data["message"]["venue"];
-      $this->poll = $data["message"]["poll"];
-      $this->sticker = $data["message"]["sticker"];
-
-      return $data;
     }
 
     public function doAction(string $type = "getMe", array $data, bool $resultobj = false)
@@ -96,16 +43,25 @@
               return $result["result"];
             }
             else{
-              // $eMethods = [
-              //   "getMe" => "User",
-              //   "getUserProfilePhotos" => "UserProfilePhotos",
-              //   "getFile" => "File",
-              //   "getChat" => "Chat",
-              //   "getChatMember" => "ChatMember",
-              //   "getStickerSet" => "\Stickers\StickerSet"
-              // ];
-
-              // return (array_key_exists($type, $eMethods)) ? new $eMethods[$type]($result["result"]) : new Entities\Message($result["result"]);
+              if ($resultobj === true)
+              {
+                if (is_bool($result["result"]) || is_numeric($result["result"]) || $type == "getChatAdministators")
+                {
+                  return $result["result"];
+                }
+                else
+                {
+                  switch ($type) {
+                    case 'getMe':
+                      return Entities\User($result["result"]);
+                    break;
+                    default:
+                      return Entities\Message($result["result"]);
+                    break;
+                  }
+                }
+              }
+              // --------
             }
           }
           else
@@ -121,155 +77,6 @@
       {
         return false;
       }
-    }
-
-    public function getUser()
-    {
-      return new Entities\User($this->from);
-    }
-
-    public function getChat()
-    {
-      return new Entities\Chat($this->chat);
-    }
-
-    public function getMessage()
-    {
-      $data = json_decode(file_get_contents("php://input"), true);
-      return new Entities\Message($data["message"]);
-    }
-
-    public function getPhoto(int $elemarray = -1) // Temporarily function
-    {
-      if ($elemarray >= 0)
-      {
-        return new Entities\PhotoSize($this->photo[$elemarray]);
-      }
-      else
-      {
-        return $this->photo;
-      }
-    }
-
-    public function getVideo()
-    {
-      return new Entities\Video($this->video);
-    }
-
-    public function getAnimation()
-    {
-      return new Entities\Animation($this->animation);
-    }
-
-    public function getDocument()
-    {
-      return new Entities\Document($this->document);
-    }
-
-    public function getAudio()
-    {
-      return new Entities\Audio($this->audio);
-    }
-
-    public function getVoice()
-    {
-      return new Entities\Voice($this->voice);
-    }
-
-    public function getContact()
-    {
-      return new Entities\Contact($this->contact);
-    }
-
-    public function getVenue()
-    {
-      return new Entities\Venue($this->Venue);
-    }
-
-    public function getLocation()
-    {
-      return new Entities\Location($this->location);
-    }
-
-    public function getVideoNote()
-    {
-      return new Entities\VideoNote($this->videonote);
-    }
-
-    public function getPoll()
-    {
-      return new Entities\Poll($this->poll);
-    }
-
-    public function getSticker()
-    {
-      return new Entities\Stickers\Sticker($this->sticker);
-    }
-
-    public function getText()
-    {
-      return $this->text;
-    }
-
-    public function getMessageID()
-    {
-      return $this->message_id;
-    }
-
-    public function getUpdateID()
-    {
-      return $this->update_id;
-    }
-
-    public function getUserID()
-    {
-      return $this->user_id;
-    }
-
-    public function getChatID()
-    {
-      return $this->chat_id;
-    }
-
-    public function getDate()
-    {
-      return $this->date;
-    }
-
-    public function typeMessage()
-    {
-      foreach (Methods::$types as $type)
-      {
-        if (!is_null($this->$type))
-        {
-          return $type;
-        }
-      }
-    }
-
-    public function doChatAction($chat_id, string $action)
-    {
-      self::doAction("sendChatAction", ["chat_id" => $chat_id, "action" => $action]);
-    }
-
-    public function forwardMessage($chat_id, $from_chat_id, bool $disable_notification, int $message_id)
-    {
-      return self::doAction("forwardMessage", ["chat_id" => $chat_id, "from_chat_id" => $from_chat_id, "disable_notification" => $disable_notification, "message_id" => $message_id]);
-    }
-
-    public function getFile(string $file_id)
-    {
-      return new Entities\File(self::doAction("getFile", ["file_id" => $file_id]));
-    }
-
-    public function getUserProfilePhotos(array $data)
-    {
-      return new Entities\UserProfilePhotos(self::doAction("getUserProfilePhotos", $data));
-    }
-
-    public function getMe()
-    {
-      return new Entities\User(self::doAction("getMe", []));
     }
   }
 
